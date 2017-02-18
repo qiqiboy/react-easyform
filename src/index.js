cce2d30e4022af40c48d99cce9a581ee7b52bdb0
qiqiboy <imqiqiboy@gmail.com>
2017-02-18 17:02:16 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import 'object-assign';
 export { default as Field } from './Field';
 export { default as FieldGroup } from './FieldGroup';
 export { default as easyField } from './easyFieldWrapper';
